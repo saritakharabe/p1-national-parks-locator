7d087c5c929ba3e5cfe1124efe6f3115fd89e8b7
CrowTrooper202 <adamb88cke@gmail.com>
2023-04-05 10:25:25 -0400

    moved some js code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 
-// -----------------------------------------------------------------------------------------------
-// National parks API call: need to replace STATE with the results of the state pulldown menu; returns lat and lon by park
-// var STATE = document.selectArtibute('.dropdown-item')
-// var apiKey = "6Nd3QpLEE05BdiKDL5AMEu9GVW08cRlapS83eKaQ"
-// var apiCall = 'https://developer.nps.gov/api/v1/parks?stateCode=' + STATE + '&api_key=' + apiKey
-
-// fetch(apiCall)
-
-// .then(function (response) {
-//     console.log(response);
-//     return response.json();
-
-// }) 
-// .then(function(data) {
-//     console.log(data);
-// })
-
 
 //variable for map box API
 var mapKey = "pk.eyJ1IjoiYXNiMjAyIiwiYSI6ImNsZzJkcmxudTA0aDkzdHFzMHh1Mzk0a3cifQ.HQAX2YLQBEZjcagat-k8vw"
@@ -28,20 +11,6 @@ const map = new mapboxgl.Map({
     zoom: 2, // starting zoom
 });
 
-//nationalParckBtn.on('click', function(){
-//    var lat = [park].place.date(lat);
-//    var long = [park].place.date(long);
-
-//    const map = new mapboxgl.Map({
-//     container: 'map', // container ID
-//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-//     center: [long, lat], // starting position [lng, lat]
-//     zoom: 9, // starting zoom
-// });
-// })
-//     center: [-74.5, 40], // starting position [lng, lat]
-//     zoom: 9, // starting zoom
-// });
 
 map.on('load', () => {
     // Load an image from an external URL.
@@ -77,7 +46,7 @@ map.on('load', () => {
                 'source': 'point', // reference the data source
                 'layout': {
                     'icon-image': 'cat', // reference the image
-                    'icon-size': 0.01
+                    'icon-size': 0.02
                 }
             });
         }
@@ -85,20 +54,35 @@ map.on('load', () => {
 });
 
 // National parks API call: need to replace STATE with the results of the state pulldown menu; returns lat and lon by park
-// var STATE = document.selectArtibute('.dropdown-item')
-// var apiKey = "6Nd3QpLEE05BdiKDL5AMEu9GVW08cRlapS83eKaQ"
-// var apiCall = 'https://developer.nps.gov/api/v1/parks?stateCode=' + STATE + '&api_key=' + apiKey
+var STATE = document.selectArtibute('.dropdown-item')
+var apiKey = "6Nd3QpLEE05BdiKDL5AMEu9GVW08cRlapS83eKaQ"
+var apiCall = 'https://developer.nps.gov/api/v1/parks?stateCode=' + STATE + '&api_key=' + apiKey
 
-// fetch(apiCall)
+fetch(apiCall)
 
-// .then(function (response) {
-//     console.log(response);
-//     return response.json();
+.then(function (response) {
+    console.log(response);
+    return response.json();
 
-// }) 
-// .then(function(data) {
-//     console.log(data);
+}) 
+.then(function(data) {
+    console.log(data);
+})
+
+//nationalParckBtn.on('click', function(){
+//    var lat = [park].place.date(lat);
+//    var long = [park].place.date(long);
+
+//    const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: [long, lat], // starting position [lng, lat]
+//     zoom: 9, // starting zoom
+// });
 // })
+//     center: [-74.5, 40], // starting position [lng, lat]
+//     zoom: 9, // starting zoom
+// });
 
 var clickDropdown = document.addEventListener('DOMContentLoaded', function () {
     var dropdown = document.querySelector('.dropdown');
