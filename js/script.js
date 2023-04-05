3bcc065285e13b959a5af2a3f4a08ea1d5ec6ab1
CrowTrooper202 <adamb88cke@gmail.com>
2023-04-05 13:51:28 -0400

    condensed 'zoom' code and updated local storage code

--- js/script.js
+++ js/script.js
@@ -117,11 +117,11 @@ for (var i = 0; i < 10; i++) {
 
 var container = document.querySelector("#search-results")
 var container1 = document.querySelector('#saved-serchers')
-container.addEventListener('click', function (event) {
-    //code to replace standard map
+
+function mapZoom(event) {
     var element = event.target;
-    var lng = element.getAttribut('longatude')
-    var lat = element.getAttribute('latatude')
+    var lng = element.getAttribut('data-lng')
+    var lat = element.getAttribute('data-lat')
 
     if(element.matches('.parkcard')){
     mapboxgl.accessToken = mapKey;
@@ -173,72 +173,29 @@ container.addEventListener('click', function (event) {
     });
     }  
 
-})
+}
 
-container1.addEventListener('click', function (event) {
-    //code to replace standard map
-    var element = event.target;
-    var lng = element.getAttribut('longatude')
-    var lat = element.getAttribute('latatude')
-
-    if(element.matches('.parkcard')){
-    mapboxgl.accessToken = mapKey;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: [lng, lat], // starting position [lng, lat]
-        zoom: 4, // starting zoom
-    });
-    map.on('load', () => {
-        // Load an image from an external URL.
-        map.loadImage(
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Logo_of_the_United_States_National_Park_Service.svg/1200px-Logo_of_the_United_States_National_Park_Service.svg.png',
-            (error, image) => {
-                if (error) throw error;
+container.addEventListener('click', mapZoom() )
     
-                // Add the image to the map style.
-                map.addImage('logo', image);
-    
-                // Add a data source containing one point feature.
-                map.addSource('point', {
-                    'type': 'geojson',
-                    'data': {
-                        'type': 'FeatureCollection',
-                        'features': [
-                            {
-                                'type': 'Feature',
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [lng, lat]
-                                }
-                            }
-                        ]
-                    }
-                });
-    
-                // Add a layer to use the image to represent the data.
-                map.addLayer({
-                    'id': 'points',
-                    'type': 'symbol',
-                    'source': 'point', // reference the data source
-                    'layout': {
-                        'icon-image': 'logo', // reference the image
-                        'icon-size': 0.02
-                    }
-                });
-            }
-        );
-    });
-    }  
-
-})
+container1.addEventListener('click', mapZoom())
 //---------> code for 'zoom' function above<----------------------//
 
 //--------->Local Storage to fallow<--------------------//
 container.addEventListener('click', function (event) {
-    event.preventDefault()
     var element = event.target;
-    localStorage.setItem('parkcard', element)
+
+    if (Element.matches('parkcard')) {
+        var parkName = event.textContent;
+        var lng = element.getAttribut('data-lng')
+        var lat = element.getAttribute('data-lat')
+        var parkObj = {
+            parkName: parkName,
+            longatude: lng,
+            latatude: lat,
+        }
+        localStorage.setItem('parkcard', parkObj)
+    }
+    
 })
 
 renderLastSlected()
