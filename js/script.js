fdd03097f54b26cdcd30987755959ce1900ce285
CrowTrooper202 <adamb88cke@gmail.com>
2023-04-06 15:31:41 -0400

    finished local storage

--- js/script.js
+++ js/script.js
@@ -22,40 +22,40 @@ const map = new mapboxgl.Map({
 var searchResults = document.querySelector("#search-results");
 
 function displayParkList(data) {
-  var length = data.data.length; //10
-  searchResults.innerHTML = " ";
-  if (length > 10) {
-    length = 10;
-  }
-  for (var i = 0; i < length; i++) {
-    searchResults.style.cssText =
-      "display: flex; flex-wrap: wrap; justify-content: center";
-
-    var parkCard = document.createElement("card");
-    parkCard.classList.add('park-card');
-
-    var cardTitle = document.createElement("h3");
-    cardTitle.innerHTML = data.data[i].name;
-    cardTitle.dataset.lat = data.data[i].latitude;
-    cardTitle.dataset.lon = data.data[i].longitude;
-    cardTitle.dataset.code = data.data[i].parkCode;
-    cardTitle.dataset.name = data.data[i].name;
-    cardTitle.style.cssText =
-      "font-weight: bold; cursor: grab; text-decoration: underline; color: #00308F";
-
-    var cardState = document.createElement("h3");
-    cardState.innerHTML = "State(s): " + data.data[i].states;
-    var cardActivities = document.createElement("h3");
-    cardActivities.innerHTML = "Some of the Available Activities:";
-    parkCard.appendChild(cardTitle);
-    parkCard.appendChild(cardState);
-    parkCard.appendChild(cardActivities);
-
-    var activity = data.data[i].activities.length;
-    if (activity > 5) {
-      activity = 5;
+    var length = data.data.length; //10
+    searchResults.innerHTML = " ";
+    if (length > 10) {
+        length = 10;
     }
+    for (var i = 0; i < length; i++) {
+        searchResults.style.cssText =
+            "display: flex; flex-wrap: wrap; justify-content: center";
+
+        var parkCard = document.createElement("card");
+        parkCard.classList.add('park-card');
+
+        var cardTitle = document.createElement("h3");
+        cardTitle.innerHTML = data.data[i].name;
+        cardTitle.dataset.lat = data.data[i].latitude;
+        cardTitle.dataset.lon = data.data[i].longitude;
+        cardTitle.dataset.code = data.data[i].parkCode;
+        cardTitle.dataset.nameP = data.data[i].name;
+        cardTitle.style.cssText =
+            "font-weight: bold; cursor: grab; text-decoration: underline; color: #00308F";
+
+        var cardState = document.createElement("h3");
+        cardState.innerHTML = "State(s): " + data.data[i].states;
+        var cardActivities = document.createElement("h3");
+        cardActivities.innerHTML = "Some of the Available Activities:";
+        parkCard.appendChild(cardTitle);
+        parkCard.appendChild(cardState);
+        parkCard.appendChild(cardActivities);
+
+        var activity = data.data[i].activities.length;
+        if (activity > 5) {
+            activity = 5;
         }
+
         for (var j = 0; j < activity; j++) {
             var cardList = document.createElement("ul");
             var actList1 = document.createElement("li");
@@ -66,186 +66,189 @@ function displayParkList(data) {
         searchResults.appendChild(parkCard);
         cardTitle.addEventListener("click", expandDetails);
     }
+}
 
-// -------------------------------------------------------------------------------------------
-// When a search result is clicked, expands to additional details about the park
-var expandDetails = function (park) {
-    var element = park.target;
-    console.log(element)
+    // -------------------------------------------------------------------------------------------
+    // When a search result is clicked, expands to additional details about the park
+    var expandDetails = function (park) {
+        var element = park.target;
+        console.log(element)
 
-    var api = "https://developer.nps.gov/api/v1/parks?parkCode=" + element.dataset.code + "&api_key=" +
-    apiKey;
+        var api = "https://developer.nps.gov/api/v1/parks?parkCode=" + element.dataset.code + "&api_key=" +
+            apiKey;
 
-    // new API fetch
-    fetch(api)
-    .then(function (parkResponse) {
-      return parkResponse.json()
-      
-      .then(function (parkResponse) {
-        searchResults.innerHTML = " ";
+        // new API fetch
+        fetch(api)
+            .then(function (parkResponse) {
+                return parkResponse.json()
 
-        // creates new card with neccesary details
-        var detailedCard = document.createElement("div");
-        detailedCard.classList.add('detailed-card')
-      
-        var detailedTitle = document.createElement("h1");
-        detailedTitle.innerHTML = parkResponse.data[0].name;
-      
-        var detailedDescription = document.createElement("p");
-        detailedDescription.innerHTML = parkResponse.data[0].description;
+                    .then(function (parkResponse) {
+                        searchResults.innerHTML = " ";
 
-        var cardList = document.createElement("ul")
-        cardList.innerHTML = "The activities available at this park include: "
-        var activity = parkResponse.data[0].activities.length;
-        
-        for (var j = 0; j < activity; j++) {
-          var actList1 = document.createElement("li");
-          actList1.innerHTML = parkResponse.data[0].activities[j].name;
-          cardList.appendChild(actList1);
-        }
+                        // creates new card with neccesary details
+                        var detailedCard = document.createElement("div");
+                        detailedCard.classList.add('detailed-card')
 
-        var detailedURL = document.createElement("p");
-        detailedURL.innerHTML =
-          "For more information, visit the park's page at: " + parkResponse.data[0].url;
-      
-        detailedCard.appendChild(detailedTitle);
-        detailedCard.appendChild(detailedDescription);
-        detailedCard.appendChild(cardList);
-        detailedCard.appendChild(detailedURL);
-        searchResults.appendChild(detailedCard);
-     }) });
-      
-      }
+                        var detailedTitle = document.createElement("h1");
+                        detailedTitle.innerHTML = parkResponse.data[0].name;
+
+                        var detailedDescription = document.createElement("p");
+                        detailedDescription.innerHTML = parkResponse.data[0].description;
+
+                        var cardList = document.createElement("ul")
+                        cardList.innerHTML = "The activities available at this park include: "
+                        var activity = parkResponse.data[0].activities.length;
+
+                        for (var j = 0; j < activity; j++) {
+                            var actList1 = document.createElement("li");
+                            actList1.innerHTML = parkResponse.data[0].activities[j].name;
+                            cardList.appendChild(actList1);
+                        }
+
+                        var detailedURL = document.createElement("p");
+                        detailedURL.innerHTML =
+                            "For more information, visit the park's page at: " + parkResponse.data[0].url;
+
+                        detailedCard.appendChild(detailedTitle);
+                        detailedCard.appendChild(detailedDescription);
+                        detailedCard.appendChild(cardList);
+                        detailedCard.appendChild(detailedURL);
+                        searchResults.appendChild(detailedCard);
+                    })
+            });
+
+    }
+        ;
+
+
     ;
 
-  
-;
+    // showing state-code dropdown list
+    var dropdown = document.querySelector(".dropdown");
+    var clickdropdownmenu = document.addEventListener(
+        "DOMContentLoaded",
+        function () {
+            dropdown.addEventListener("click", function (event) {
+                event.stopPropagation();
+                dropdown.classList.toggle("is-active");
+            });
+        }
+    );
 
-// showing state-code dropdown list
-var dropdown = document.querySelector(".dropdown");
-var clickdropdownmenu = document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-        dropdown.addEventListener("click", function (event) {
-            event.stopPropagation();
-            dropdown.classList.toggle("is-active");
-        });
-    }
-);
-
-var removeDropdownList = document.addEventListener("click", function () {
-    dropdown.classList.remove("is-active");
-});
-
-fetch("./assets/statecodes.json")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // console.log(data);
-        showdropdownList(data);
+    var removeDropdownList = document.addEventListener("click", function () {
+        dropdown.classList.remove("is-active");
     });
-function showdropdownList(data) {
-    var dropdownList = document.getElementById("dropdown-menu");
-    for (var i = 0; i < data.length; i++) {
-        var divEl = document.createElement("div");
-        divEl.value = data[i].code;
-        divEl.addEventListener("click", onStateSelect);
-        divEl.innerHTML = data[i].code;
-        dropdownList.appendChild(divEl);
-    }
-}
 
-var parkname = [];
-function onStateSelect(event) {
-    var spanEl = document.getElementById("state-code");
-    spanEl.innerHTML = event.target.value;
-    var api = apiCall.replace("{0}", event.target.value);
-
-    fetch(api)
-        .then(function (parkResponse) {
-            if (parkResponse.ok) {
-                console.log(parkResponse);
-                parkResponse.json().then(function (data) {
-                    console.log(data);
-                    displayParkList(data);
-                });
-            } else {
-                alert("park API error");
-            }
+    fetch("./assets/statecodes.json")
+        .then(function (response) {
+            return response.json();
         })
-        .catch(function (err) {
-            console.log(err);
+        .then(function (data) {
+            // console.log(data);
+            showdropdownList(data);
         });
-}
-
-// Code to expand the tile to show more details on clicked park
-//variable for map box API
-
-//after filtering activity showing park list
-
-var container = document.querySelector("#search-results");
-var container1 = document.querySelector(".saved-parks");
-
-function mapZoom(event) {
-    var element = event.target;
-    if (element.matches("h3")) {
-        var lng = element.getAttribute("data-lon");
-        var lat = element.getAttribute("data-lat");
-
-        // if (element.matches('.parkcard')) {
-        mapboxgl.accessToken = mapKey;
-        const map = new mapboxgl.Map({
-            container: "map", // container ID
-            style: "mapbox://styles/mapbox/streets-v12", // style URL
-            center: [lng, lat], // starting position [lng, lat]
-            zoom: 6, // starting zoom
-        });
-        map.on("load", () => {
-            // Load an image from an external URL.
-            map.loadImage(
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Logo_of_the_United_States_National_Park_Service.svg/1200px-Logo_of_the_United_States_National_Park_Service.svg.png",
-                (error, image) => {
-                    if (error) throw error;
-
-                    // Add the image to the map style.
-                    map.addImage("logo", image);
-
-                    // Add a data source containing one point feature.
-                    map.addSource("point", {
-                        type: "geojson",
-                        data: {
-                            type: "FeatureCollection",
-                            features: [
-                                {
-                                    type: "Feature",
-                                    geometry: {
-                                        type: "Point",
-                                        coordinates: [lng, lat],
-                                    },
-                                },
-                            ],
-                        },
-                    });
-
-                    // Add a layer to use the image to represent the data.
-                    map.addLayer({
-                        id: "points",
-                        type: "symbol",
-                        source: "point", // reference the data source
-                        layout: {
-                            "icon-image": "logo", // reference the image
-                            "icon-size": 0.02,
-                        },
-                    });
-                }
-            );
-        });
-        // }
+    function showdropdownList(data) {
+        var dropdownList = document.getElementById("dropdown-menu");
+        for (var i = 0; i < data.length; i++) {
+            var divEl = document.createElement("div");
+            divEl.value = data[i].code;
+            divEl.addEventListener("click", onStateSelect);
+            divEl.innerHTML = data[i].code;
+            dropdownList.appendChild(divEl);
+        }
     }
-}
 
-container.addEventListener("click", mapZoom);
+    var parkname = [];
+    function onStateSelect(event) {
+        var spanEl = document.getElementById("state-code");
+        spanEl.innerHTML = event.target.value;
+        var api = apiCall.replace("{0}", event.target.value);
+
+        fetch(api)
+            .then(function (parkResponse) {
+                if (parkResponse.ok) {
+                    console.log(parkResponse);
+                    parkResponse.json().then(function (data) {
+                        console.log(data);
+                        displayParkList(data);
+                    });
+                } else {
+                    alert("park API error");
+                }
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+
+    // Code to expand the tile to show more details on clicked park
+    //variable for map box API
+
+    //after filtering activity showing park list
+
+    var container = document.querySelector("#search-results");
+    var container1 = document.querySelector(".saved-parks");
+
+    function mapZoom(event) {
+        var element = event.target;
+        console.log(element);
+        if (element.matches("h3")) {
+            var lng = element.getAttribute("data-lon");
+            var lat = element.getAttribute("data-lat");
+
+            // if (element.matches('.parkcard')) {
+            mapboxgl.accessToken = mapKey;
+            const map = new mapboxgl.Map({
+                container: "map", // container ID
+                style: "mapbox://styles/mapbox/streets-v12", // style URL
+                center: [lng, lat], // starting position [lng, lat]
+                zoom: 6, // starting zoom
+            });
+            map.on("load", () => {
+                // Load an image from an external URL.
+                map.loadImage(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Logo_of_the_United_States_National_Park_Service.svg/1200px-Logo_of_the_United_States_National_Park_Service.svg.png",
+                    (error, image) => {
+                        if (error) throw error;
+
+                        // Add the image to the map style.
+                        map.addImage("logo", image);
+
+                        // Add a data source containing one point feature.
+                        map.addSource("point", {
+                            type: "geojson",
+                            data: {
+                                type: "FeatureCollection",
+                                features: [
+                                    {
+                                        type: "Feature",
+                                        geometry: {
+                                            type: "Point",
+                                            coordinates: [lng, lat],
+                                        },
+                                    },
+                                ],
+                            },
+                        });
+
+                        // Add a layer to use the image to represent the data.
+                        map.addLayer({
+                            id: "points",
+                            type: "symbol",
+                            source: "point", // reference the data source
+                            layout: {
+                                "icon-image": "logo", // reference the image
+                                "icon-size": 0.02,
+                            },
+                        });
+                    }
+                );
+            });
+            // }
+        }
+    }
+
+    container.addEventListener("click", mapZoom);
 
 container1.addEventListener("click", mapZoom);
 
@@ -253,6 +256,7 @@ container1.addEventListener("click", mapZoom);
 
 container.addEventListener('click', function (event) {
     var element = event.target;
+    console.log(element);
     var currentParks = JSON.parse(localStorage.getItem('parkcard'))
     if (!currentParks) {
         currentParks = []
@@ -276,31 +280,21 @@ container.addEventListener('click', function (event) {
             if (!parks) {
                 return;
             }
-            // container1.textContent = parks.parkName
-            // searchResults.style.cssText =
-            //     "display: flex; flex-wrap: wrap; justify-content: center";
+            container1.textContent = parks.parkName
+            searchResults.style.cssText =
+                "display: flex; flex-wrap: wrap; justify-content: center";
             for (let i = 0; i < parks.length; i++) {
-                var parkCard = document.createElement("div");
+                var parkCard = document.createElement("h3");
                 parkCard.textContent = parks[i].parkName
-                parkCard.setAttribute('data-lng', lng)
-                parkCard.setAttribute('data-lat', lat)
+                parkCard.setAttribute('data-lon', parks[i].longatude)
+                parkCard.setAttribute('data-lat', parks[i].latatude)
                 parkCard.style.cssText =
                     "border: 2px solid #000000; margin: 10px; padding: 10px; width: 45%; background-color: #fcfcf4; border-radius: 10px";
                 parkCard.classList.add("park-card");
                 document.querySelector(".saved-parks").append(parkCard)
 
-
             }
 
-
-
-            // var cardTitle = document.createElement("h3");
-            // cardTitle.innerHTML = data.data[i].name;
-            // cardTitle.dataset.lat = data.data[i].latitude;
-            // cardTitle.dataset.lon = data.data[i].longitude;
-            // cardTitle.dataset.code = data.data[i].parkCode;
-            // cardTitle.dataset.nameP = data.data[i].name;
-            // cardTitle.style.cssText = "font-weight: bold; cursor: grab; text-decoration: underline; color: #00308F";
         }
         renderLastSlected()
     }
